e1530cb7012b7f177fd90ed3fcf0e22324aa4e80
Aziz Chikh <azizchpay99@outlook.com>
2023-07-06 09:38:23 +0200

    Add seperate .py file for implementing the custom CNN model. Still dummy example till now

---
+++ CustomCNN.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+#dummy example --> needs to be checked and improved
+class CNNModel(nn.Module):
+    def __init__(self, num_classes):
+        super(CNNModel, self).__init__()
+
+        self.cnn_network = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.cnn_network(x)
