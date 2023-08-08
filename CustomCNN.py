a7f4b80f202a94bd5d163ca26a9ff0eb80267b47
Aziz Chikh <azizchpay99@outlook.com>
2023-08-08 19:59:01 +0200

    Implement CNN [TAKE 2]

--- CustomCNN.py
+++ CustomCNN.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 #dummy example --> needs to be checked and improved
 class CNNModel(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self):
         super(CNNModel, self).__init__()
 
-        self.cnn_network = nn.Sequential(
+        """self.cnn_network = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
@@ -21,4 +21,34 @@ class CNNModel(nn.Module):
         self.flatten = nn.Flatten()
 
     def forward(self, x):
-        return self.cnn_network(x)
+        return self.cnn_network(x)"""
+        self.network = nn.Sequential(
+            
+            nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+        
+            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Flatten(),
+            nn.Linear(82944,1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512,6)
+        )
+    
+    def forward(self, xb):
+        return self.network(xb)
