c7517c71a3a9f114b502022ca3ac5b1959f0cf23
Aziz Chikh <azizchpay99@outlook.com>
2023-08-12 14:29:12 +0200

    Modify the CNN model implementation

--- CustomCNN.py
+++ CustomCNN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 #dummy example --> needs to be checked and improved
 class CNNModel(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(CNNModel, self).__init__()
 
         """self.cnn_network = nn.Sequential(
@@ -22,7 +22,7 @@ class CNNModel(nn.Module):
 
     def forward(self, x):
         return self.cnn_network(x)"""
-        self.network = nn.Sequential(
+        """self.network = nn.Sequential(
             
             nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
             nn.ReLU(),
@@ -48,7 +48,20 @@ class CNNModel(nn.Module):
             nn.Linear(1024, 512),
             nn.ReLU(),
             nn.Linear(512,6)
-        )
-    
-    def forward(self, xb):
-        return self.network(xb)
+        )"""
+        self.num_classes = num_classes
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(16 * 90 * 90, 64)
+        self.fc2 = nn.Linear(64, num_classes)  # num_classes is the number of output classes
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
